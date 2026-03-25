34881f617fa631598eae733d805a0757daf7d5c2
fabiolog <fabio.ballasina@gmail.com>
2026-03-25 16:56:40 +0100

    Update Astro config with site and base settings
    Added site and base configuration for Astro project.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+// https://astro.build/configexport default defineConfig({});
+export default defineConfig({
+  site: 'https://fabiolog.github.io',
+  base: '/io', // IMPORTANTE: Metti il nome della tua repo con lo slash davanti
+});
